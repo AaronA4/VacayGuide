2f00b3fbd0cab95535fc97eeac0bb6502ad6dda7
GregoryBBaumann <100977612+GregoryBBaumann@users.noreply.github.com>
2022-11-19 18:24:36 -0500

    Initial Skeleton
    Init Skeleton using class examples

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const app = require('express');
+const http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('new client connected', socket.id);
+
+  socket.on('user_join', (name) => {
+    console.log('A user joined their name is ' + name);
+    socket.broadcast.emit('user_join', name);
+  });
+
+  socket.on('message', ({name, message}) => {
+    console.log(name, message, socket.id);
+    io.emit('message', {name, message});
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Disconnect Fired');
+  });
+});
+
+http.listen(4000, () => {
+  console.log(`listening on *:${4000}`);
+});
