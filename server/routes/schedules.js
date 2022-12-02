1b8d1966a3a000baeb74175341ca5b222d8cda6d
nwhitt23 <nfwhitt3901@gmail.com>
2022-12-01 19:32:49 -0500

    Set up route for chatrooms

--- server/routes/schedules.js
+++ server/routes/schedules.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { isObject } = require('util');
 const router = express.Router();
 const data = require('../data');
 const scheduleData = data.schedules;
-
+const server = require('http').createServer(express);
+var io = require('socket.io')(server);
 
 router.get('/', async (req,res) => {
     try{
@@ -33,6 +35,40 @@ router.post('/', async (req,res) => {
     }
 
 
+});
+
+router.get('/:scheduleId/chat', async (req,res) => {
+    if (!req.params.scheduleId) {
+        res.status(400).json({ error: 'Schedule ID not provided.' });
+    };
+    // user validation req
+    try {
+        io.on('connection', (socket) => {
+            console.log('New client connected.', socket.id);
+
+            socket.on('user_join', ({name,event}) => {
+                console.log('User '+ name +' has joined room '+ event +'.'); // State may possibly include 'group' variable for different groups going to same event.
+                socket.join(room);
+                socket.to(room).emit('user_join', name);
+            });
+
+            socket.on('message', ({name, message, room}) => {
+                // console.log(name, message, socket.id, room);
+                io.to(room).emit('message', {name, message});
+            });
+
+            socket.on('disconnect', ({name,room}) => {
+                console.log('User '+ name +' has left room '+ room +'.');
+                socket.to(room).emit('disconnect', name);
+            });
+        });
+
+        server.listen(4000, () => {
+            console.log(`Listening on *:${4000}`);
+        });
+    } catch (e) {
+        res.status(400).json({ error: 'Failed connection.' });
+    }
 });
 
 module.exports = router;
