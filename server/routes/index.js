b46a96e595ce165b8b9f4b307542080ec8fc6969
Naveen-Sai-Ganadi <105182393+Naveen-Sai-Ganadi@users.noreply.github.com>
2022-12-05 12:50:22 -0500

    Accepting and viewing invites

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const scheduleRoutes = require('./schedules');
+const inviteRoutes = require('./invites');
 
 const constructorMethod = (app) => {
     app.use('/schedules', scheduleRoutes);  
-    
+    app.use('/invites', inviteRoutes);
     app.use('*', (req,res) => {
         res.sendStatus(404);
     });
--- server/tasks/seed.js
+++ server/tasks/seed.js
@@ -3,16 +3,25 @@ const dbConnection = require('../config/mongoConnection');
 const data = require('../data/');
 const schedules = data.schedules;
 const users = data.users;
+const invites = data.invites;
 
 async function main() {
   try{
     const db = await dbConnection.dbConnection();
-    await db.dropDatabase();
+    // await db.dropDatabase();
   
-    let user1 =  await users.addUser("rahul@xyz.com","Rahul","Ray","rahul@1");
-    let user2 = await users.addUser("sam@xyz.com","Sam","Ray","sam@1");
+    // let user1 =  await users.addUser("rahul@xyz.com","Rahul","Ray","rahul@1");
+    // let user2 = await users.addUser("sam@xyz.com","Sam","Ray","sam@1");
 
-    await schedules.addSchedule('Long Trip', user1.createdUser._id.toString(),[],[]);
+    // await schedules.addSchedule('Long Trip', user1.createdUser._id.toString(),[],[]);
+
+    let invite = {
+      senderId: "638e235b97ea22d6e1b281ad",
+      scheduleId: "638e235b97ea22d6e1b281af"
+
+    }
+
+    await users.addInvite("638e235b97ea22d6e1b281ae", invite);
     
     
     console.log('Done seeding database');
