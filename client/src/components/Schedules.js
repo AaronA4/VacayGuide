ad1b491ebc71bcb8a4593e38d98013c645f9a09a
nwhitt23 <nfwhitt3901@gmail.com>
2022-12-18 05:50:18 -0500

    Implemented Schedule, Schedules, AddSchedule components

--- client/src/components/Schedules.js
+++ client/src/components/Schedules.js
@@ -4,63 +4,67 @@ import {Link} from 'react-router-dom';
 import axios from 'axios';
 
 function Schedules() {
-    const [loading, setLoading] = useState(true);
-    const [schedulesData, setSchedulesData] = useState(undefined);
-    let list = null;
+  const [loading, setLoading] = useState(true);
+  const [schedulesData, setSchedulesData] = useState(undefined);
+  const [addBtnToggle, setBtnToggle] = useState(false);
+  let list = null;
 
-    useEffect(() => {
-      console.log('on load useEffect');
-      async function fetchData() {
-        try {
-          const { data } = await axios.get('http://localhost:3000/schedules/');
-          setSchedulesData(data);
-          setLoading(false);
-        } catch (e) {
-          console.log(e);
-        }
-      };
-      fetchData();
-    }, []);
-    const buildCard = (schedule) => {
-      return (
-        <Link to={`/schedules/${schedule.id}`}>
-          <div id={schedule.id} class="card col-lg-3 col-md-6 col-sm-12">
-            {/* <img class="card-img-top" src="IMG SOURCE HERE" alt="scheduleImg"/> */}
-            <div class="card-body">
-              <h2 class="card-title">{schedule.name}</h2>
-              <h4 class="card-subtitle">{schedule.creator}</h4>
-              <span class="card-text">More Info</span>
-            </div>
-          </div>
-        </Link>
-      )
-    }
-
-    list = 
-      schedulesData 
-      && schedulesData.map((schedule) => {
-        return buildCard(schedule);
-      });
-
-    if (loading) {
-      return (
-        <div>
-          <h2>Loading. . . .</h2>
-        </div>
-      );
-    } else {
-      return (
-        <div class="content">
-          <br />
-          <h1>This is where the list of schedules will be displayed.</h1>
-          <div class="container">
-            <div class="row">
-              {list}
-            </div>
+  useEffect(() => {
+    console.log('on load useEffect');
+    async function fetchData() {
+      try {
+        const { data } = await axios.get('http://localhost:3000/schedules/');
+        setSchedulesData(data);
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
+  }, []);
+  
+  const buildCard = (schedule) => {
+    return (
+      <Link to={`/schedules/${schedule.id}`}>
+        <div id={schedule.id} class="card col-lg-3 col-md-6 col-sm-12">
+          {/* <img class="card-img-top" src="IMG SOURCE HERE" alt="scheduleImg"/> */}
+          <div class="card-body">
+            <h2 class="card-title">{schedule.name}</h2>
+            <span class="card-text">More Info</span>
           </div>
         </div>
-      );
-    }
+      </Link>
+    )
+  }
+
+  list = 
+    schedulesData 
+    && schedulesData.map((schedule) => {
+      return buildCard(schedule);
+    });
+
+  if (loading) {
+    return (
+      <div>
+        <h2>Loading. . . .</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div class="content">
+        <br />
+        <h1>Current Vacay Schedules</h1>
+        <br />
+        <button onClick={()=> setBtnToggle(!addBtnToggle)}>Create Schedule</button>
+        <br />
+        <div class="container">
+          <div class="row">
+            {list}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
   
 export default Schedules;
