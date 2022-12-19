b461126d1a9c189631f54957b9dd782f355ee9b5
AaronA4 <aalfaro2@stevens.edu>
2022-12-19 16:34:30 -0500

    fixing events

--- client/src/components/CreateEvent.js
+++ client/src/components/CreateEvent.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 import {AuthContext} from '../firebase/Auth';
 import { checkString, checkCost, checkDate } from '../validation.js';
 
-function EditEvent() {
+function CreateEvent() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [customError, setCustomError] = useState(undefined);
@@ -15,7 +15,7 @@ function EditEvent() {
   const [validated, setValidated] = useState(false);
   const {currentUser} = useContext(AuthContext);
   const navigate = useNavigate();
-  let {scheduleId, eventId} = useParams();
+  let {scheduleId} = useParams();
 
   useEffect(() => {
     console.log('on load useEffect');
@@ -46,31 +46,27 @@ function EditEvent() {
     let startTime;
     let endTime;
 
-    let eventData = scheduleData.events.find(event => event.name == eventId);
 
-    try{
-      if (form.name.value) {
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+      flag = false;
+    }else {
+      try{
         name = checkString(form.name.value, 'Name');
         for (let event of scheduleData.events) {
           if (event.name == name) throw 'Error: Cannot have two events with the same name';
         }
-      }
-      if (form.description.value) description = checkString(form.description.value, 'Description');
-      if (form.cost.value) cost = checkCost(form.cost.value, 'Cost');
-      if (form.startTime.value) {
+        description = checkString(form.description.value, 'Description');
+        cost = checkCost(form.cost.value, 'Cost');
         startTime = new Date(form.startTime.value);
         startTime = checkDate(startTime, 'Start Time');
-      }
-      if (form.endTime.value) {
         endTime = new Date(form.endTime.value);
         endTime = checkDate(endTime, 'End Time');
+        if (endTime.getTime() <= startTime.getTime()) throw 'Error: End time cannot happen before or at the same time as start time';
+      }catch(e) {
+        flag = false;
+        setCustomError(e);
       }
-      if (form.startTime.value && form.endTime.value && endTime.getTime() <= startTime.getTime()) throw 'Error: End time cannot happen before or at the same time as start time';
-      if (form.startTime.value && !form.endTime.value && eventData.endTime <= startTime.getTime()) throw 'Error: End time cannot happen before or at the same time as start time';
-      if (!form.startTime.value && form.endTime.value && endTime.getTime() <= eventData.startTime) throw 'Error: End time cannot happen before or at the same time as start time';
-    }catch(e) {
-      flag = false;
-      setCustomError(e);
     }
 
     if (flag === true){
@@ -108,39 +104,56 @@ function EditEvent() {
     }else {
       return (
         <div>
-          <p>Edit Event</p>
+          <p>Create Event</p>
           {customError && <p variant='warning'>{customError}</p>}
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Name</Form.Label>
               <Form.Control 
+                required
                 type="text" 
                 placeholder="Enter name"
               />
+              <Form.Control.Feedback type="invalid">
+                Please enter a name.
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="description">
               <Form.Label>Description</Form.Label>
               <Form.Control
+                required 
                 as="textarea" 
                 rows={3} 
                 placeholder="Enter description" 
               />   
+              <Form.Control.Feedback type="invalid">
+                Please enter a description.
+              </Form.Control.Feedback>
              </Form.Group>
 
             <Form.Group className="mb-3" controlId="cost">
               <Form.Label>Cost</Form.Label>
-              <Form.Control type="number" placeholder="Enter cost" />
+              <Form.Control required type="number" placeholder="Enter cost" />
+              <Form.Control.Feedback type="invalid">
+                Please enter a cost.
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="startTime">
               <Form.Label>Start Time</Form.Label>
-              <Form.Control type="datetime-local" placeholder="Enter start time" />
+              <Form.Control required type="datetime-local" placeholder="Enter start time" />
+              <Form.Control.Feedback type="invalid">
+                Please enter a start time.
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="endTime">
               <Form.Label>End Time</Form.Label>
-              <Form.Control type="datetime-local" placeholder="Enter end time" />
+              <Form.Control required type="datetime-local" placeholder="Enter end time" />
+              <Form.Control.Feedback type="invalid">
+                Please enter a end time.
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="image">
@@ -157,4 +170,4 @@ function EditEvent() {
     }
 }
 
-export default EditEvent;
+export default CreateEvent;
