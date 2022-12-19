3e10a5717a5f192896e30ec2f1a37071154c113b
Naveen-Sai-Ganadi <105182393+Naveen-Sai-Ganadi@users.noreply.github.com>
2022-12-19 18:16:26 -0500

    Invites Front End With Email passed in Headers
    Pending:
    Update the mail Id from session
    Validations

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import {NavLink} from 'react-router-dom';
 import {AuthContext} from '../firebase/Auth';
 import SignOutButton from './SignOut';
-import '../bootstrap/dist/css/bootstrap.css';
+// import '../bootstrap/dist/css/bootstrap.css';
 
 const Navigation = () => {
   const {currentUser} = useContext(AuthContext);
@@ -12,33 +12,33 @@ const Navigation = () => {
 const NavigationAuth = () => {
   return (
 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-       <a class="navbar-brand" href="#">Vacay Guide</a>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+       <a className="navbar-brand" href="#">Vacay Guide</a>
        <button 
-         class="navbar-toggler" 
+         className="navbar-toggler" 
          type="button" 
          data-toggle="collapse"
          aria-controls="navbarSupportedContent"
          aria-expanded="false"
          aria-label="Toggle Navigation"
        >
-         <span class="navbar-toggler-icon"></span>
+         <span className="navbar-toggler-icon"></span>
        </button>
-       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul class="navbar-nav mr-auto">
-           <li class="nav-item-active">
-             <NavLink class="nav-link" to='/'>Landing</NavLink>
+       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul className="navbar-nav mr-auto">
+           <li className="nav-item-active">
+             <NavLink className="nav-link" to='/'>Landing</NavLink>
            </li>
-           <li class="nav-item">
-             <NavLink class="nav-link" to='/home'>Home</NavLink>
+           <li className="nav-item">
+             <NavLink className="nav-link" to='/home'>Home</NavLink>
            </li>
-           <li class="nav-item">
-             <NavLink class="nav-link" to='/account'>Account</NavLink>
+           <li className="nav-item">
+             <NavLink className="nav-link" to='/account'>Account</NavLink>
            </li>
-           <li class="nav-item">
-             <NavLink class="nav-link" to='/schedules'>Schedules</NavLink>
+           <li className="nav-item">
+             <NavLink className="nav-link" to='/schedules'>Schedules</NavLink>
            </li>
-           <li class="nav-item">
+           <li className="nav-item">
             <NavLink to='/myInvites'>My Invites</NavLink>
            </li>
            <li>
