e7f0ad18fe861eeec50847f7f99c2ffcb58dc988
nwhitt23 <nfwhitt3901@gmail.com>
2022-12-19 08:09:39 -0500

    More implementation on Chatroom/Schedule routes

--- client/src/components/Schedule.js
+++ client/src/components/Schedule.js
@@ -6,15 +6,16 @@ import '../App.css';
 function Schedule(props) {
   const [loading, setLoading] = useState(true);
   const [scheduleData, setScheduleData] = useState(undefined);
-  const [showBtnToggle, setBtnToggle] = useState(false);
-  let {id} = useParams();
+  const [listBtnToggle, setListBtnToggle] = useState(false);
+  const [addBtnToggle, setAddBtnToggle] = useState(false);
+  let {scheduleId} = useParams();
   let list = null;
 
   useEffect(() => {
     console.log('Schedule useEffect')
     async function fetchData() {
       try {
-        const {data: schedule} = await axios.get(`http://localhost:3000/${id}`);
+        const {data: schedule} = await axios.get(`http://localhost:3000/${scheduleId}`);
         setScheduleData(schedule);
         setLoading(false);
       } catch (e) {
@@ -22,7 +23,21 @@ function Schedule(props) {
       }
     }
     fetchData();
-  }, [id]);
+  }, [scheduleId]);
+
+  async function userJoinEvent() {
+    try {
+      const {data} = await axios.post(`http://localhost:3000/${scheduleId}`, {
+        method: 'POST',
+        mode: 'cors',
+        body: scheduleData
+      })
+    }
+  }
+  const isEmpty = (lst) => {
+    if (lst.length === 0) return true;
+    else return false;
+  }
 
   list = 
     scheduleData
@@ -32,9 +47,20 @@ function Schedule(props) {
         <div class="card-body">
           <h4 class="card-title">{event.name}</h4>
           <h5 class="card-subtitle">{event.cost}</h5>
-          <p class="card-text">{event.desc}</p>
-          <button onClick={()=> setBtnToggle(!showBtnToggle)}>Show Attendees</button>
-          {showBtnToggle && <Attendees />}
+          <p class="card-text">Cost: {event.desc}</p>
+          <p class="card-text">Start: {event.startTime}</p>
+          <p class="card-text">End: {event.endTime}</p>
+          {/* <button onClick={()=> setListBtnToggle(!listBtnToggle)}>Show Attendees</button>
+          {(listBtnToggle && (!isEmpty(event.attendees))) 
+          && event.attendees.map((attendee) => {
+            return (
+              <div>
+                <p>{attendee}</p>
+              </div>
+            )
+          })
+          } */}
+          
         </div>
       </div>
       )
@@ -54,6 +80,7 @@ function Schedule(props) {
         <h2>{scheduleData.name}</h2>
         <div class="container">
           <h3 class="container-title">Events</h3>
+          <button onClick={() => setAddBtnToggle(!addBtnToggle)}>Add Event</button>
           <div class="row justify-content-center">
             {list}
           </div>
