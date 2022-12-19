947774e492c442701add40b96ee405d62b30953a
GregoryBBaumann <100977612+GregoryBBaumann@users.noreply.github.com>
2022-12-18 20:33:57 -0500

    Updated seed to insert user
    Seed update

--- server/tasks/seed.js
+++ server/tasks/seed.js
@@ -5,13 +5,28 @@ const schedules = data.schedules;
 const users = data.users;
 const invites = data.invites;
 
+const firebase = require('firebase');
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDgAUdZscLzqaQp6KkvlaidA1HfLA1750E",
+    authDomain: "vacayfall22.firebaseapp.com",
+    projectId: "vacayfall22",
+    storageBucket: "vacayfall22.appspot.com",
+    messagingSenderId: "43591241241",
+    appId: "1:43591241241:web:ad956bfef791758acfa488"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 async function main() {
   try{
     const db = await dbConnection.dbConnection();
     // await db.dropDatabase();
   
-    // let user1 =  await users.addUser("rahul@xyz.com","Rahul","Ray","rahul@1");
-    // let user2 = await users.addUser("sam@xyz.com","Sam","Ray","sam@1");
+    await firebase.auth().createUserWithEmailAndPassword("rahul@xyz.com", "rahul@1");
+    let user1 =  await users.addUser("rahul@xyz.com","Rahul","Ray","rahul@1");
+    await firebase.auth().createUserWithEmailAndPassword("sam@xyz.com", "sam@1");
+    let user2 = await users.addUser("sam@xyz.com","Sam","Ray","sam@1");
 
     // await schedules.addSchedule('Long Trip', user1.createdUser._id.toString(),[],[]);
 
