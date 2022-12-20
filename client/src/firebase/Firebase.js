62ca095f2eb993f9b3e3736f8ae98c2e03623115
GregoryBBaumann <100977612+GregoryBBaumann@users.noreply.github.com>
2022-12-20 18:25:11 -0500

    Fix signin/up page bug

--- client/src/firebase/Firebase.js
+++ client/src/firebase/Firebase.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-console.log(process.env.FIREBASE_API_KEY);
 const firebaseApp = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
