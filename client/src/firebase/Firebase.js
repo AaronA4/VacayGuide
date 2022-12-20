8f1240ccacfe49684af44e4eee00ce5857aacb14
GregoryBBaumann <100977612+GregoryBBaumann@users.noreply.github.com>
2022-12-19 19:12:03 -0500

    .env file fix
    env file fix

--- client/src/firebase/Firebase.js
+++ client/src/firebase/Firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-require('dotenv').config();
 
+console.log(process.env.FIREBASE_API_KEY);
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
 export default firebaseApp;
