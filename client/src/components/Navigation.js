4b08fa5754309722eb0201819de3f19744efd7ed
GregoryBBaumann <100977612+GregoryBBaumann@users.noreply.github.com>
2022-12-19 19:34:04 -0500

    Navbar changes pt2
    Updated nav bar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,51 +1,51 @@
-import React, {useContext} from 'react';
-import {NavLink} from 'react-router-dom';
-import {AuthContext} from '../firebase/Auth';
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../firebase/Auth';
 import SignOutButton from './SignOut';
 //import '/bootstrap/dist/css/bootstrap.css';
 
 const Navigation = () => {
-  const {currentUser} = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
   return <div>{currentUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>;
 };
 
 const NavigationAuth = () => {
   return (
 
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-       <a className="navbar-brand" href="#">Vacay Guide</a>
-       <button 
-         className="navbar-toggler" 
-         type="button" 
-         data-toggle="collapse"
-         aria-controls="navbarSupportedContent"
-         aria-expanded="false"
-         aria-label="Toggle Navigation"
-       >
-         <span className="navbar-toggler-icon"></span>
-       </button>
-       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul className="navbar-nav mr-auto">
-           <li className="nav-item-active">
-             <NavLink className="nav-link" to='/'>Landing</NavLink>
-           </li>
-           <li className="nav-item">
-             <NavLink className="nav-link" to='/home'>Home</NavLink>
-           </li>
-           <li className="nav-item">
-             <NavLink className="nav-link" to='/account'>Account</NavLink>
-           </li>
-           <li className="nav-item">
-             <NavLink className="nav-link" to='/schedules'>Schedules</NavLink>
-           </li>
-           <li className="nav-item">
-            <NavLink to='/myInvites'>My Invites</NavLink>
-           </li>
-           <li>
-             <SignOutButton />
-           </li>
-         </ul>
-       </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand ms-2" href="#">Vacay Guide</a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle Navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item-active">
+            <NavLink className="nav-link" to='/'>Landing</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to='/home'>Home</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to='/account'>Account</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to='/schedules'>Schedules</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to='/myInvites'>My Invites</NavLink>
+          </li>
+        </ul>
+        <div className="nav-signout me-2">
+          <SignOutButton />
+        </div>
+      </div>
     </nav>
   );
 };
@@ -53,30 +53,30 @@ const NavigationAuth = () => {
 const NavigationNonAuth = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-       <a className="navbar-brand" href="#">Vacay Guide</a>
-       <button 
-         className="navbar-toggler" 
-         type="button" 
-         data-toggle="collapse"
-         aria-controls="navbarSupportedContent"
-         aria-expanded="false"
-         aria-label="Toggle Navigation"
-       >
-         <span className="navbar-toggler-icon"></span>
-       </button>
-       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-       <ul className="navbar-nav mr-auto">
-        <li className="nav-item-active">
-          <NavLink className="nav-link" to='/'>Landing</NavLink>
-        </li>
-        <li className="nav-item-active">
-          <NavLink className="nav-link" to='/signup'>Sign-up</NavLink>
-        </li>
-        <li className="nav-item-active">
-          <NavLink className="nav-link" to='/signin'>Sign-In</NavLink>
-        </li>
-      </ul>
-       </div>
+      <a className="navbar-brand" href="#">Vacay Guide</a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle Navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item-active">
+            <NavLink className="nav-link" to='/'>Landing</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="nav-signup me-2">
+        <NavLink to='/signup'>Sign-up</NavLink>
+      </div>
+      <div className="nav-signin me-4">
+        <NavLink to='/signin'>Sign-In</NavLink>
+      </div>
     </nav>
   );
 };
